addbd72c19e0d226f52c12ef9b2283a90cb307ce
SnejUgal <contact@snejugal.ru>
2022-03-07 16:40:13 +0300

    Implement ex1

--- ex01.hpp
+++ ex01.hpp
@@ -1,8 +1,10 @@
 
-//change the below code
-// the function below now only works with integers, change it to work with any type
-// also fill out the body
-void multiples (int& sum, int x, int n)
-{
-
+// change the below code
+// the function below now only works with integers, change it to work with any
+// type also fill out the body
+template <typename T> void multiples(T &sum, T x, int n) {
+  sum = 1;
+  for (int multiple = 1; multiple <= n; multiple++) {
+    sum += multiple * x;
+  }
 }
